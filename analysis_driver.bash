5f208992229ca4c0df8a894a58e1f5ec971902dc
Casey Finnicum <casey.finnicum@coyotes.usd.edu>
2017-05-10 15:14:19 +0000

    process clean data through generating shared file

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -32,5 +32,5 @@ code/mothur/mothur code/get_good_seqs.batch
 # Run mock community data through mothur to calculate the sequencing error rates
 code/mothur/mothur code/get_error.batch
 
-
-
+#cluster the otus
+code/mothur/mothur code/get_shared_otus.batch
